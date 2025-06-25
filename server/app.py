dac42c6ddff3779b4c758efcbdd49e408cfff4ab
unknown <gohkhjustin@gmail.com>
2025-06-25 14:12:55 +0800

    app __name__

--- server/app.py
+++ server/app.py
@@ -9,4 +9,7 @@ app = Flask(__name__)
 CORS(app)
 app.register_blueprint(whyme_bp)
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
