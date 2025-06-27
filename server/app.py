341b30401c70d4172841352beda2930b0890abda
unknown <gohkhjustin@gmail.com>
2025-06-27 13:23:06 +0800

    fixed app

--- server/app.py
+++ server/app.py
@@ -42,7 +42,8 @@ def create_app():
 
     return app
 
+app = create_app()
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app = create_app()
     app.run(host="0.0.0.0", port=port, debug=False)
